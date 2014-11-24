28223912b2431aeae52be38e66f723a6363f61f8
gruemschele <news.manuelsworld@gmail.com>
2014-11-24 12:42:19 +0100

    Asks for username first

--- mtclient.py
+++ mtclient.py
@@ -1,17 +1,18 @@
 import socket
-import sys
 
-HOST, PORT = "172.16.2.118", 12345
+HOST, PORT = "localhost", 12345
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 data = ""
+name = input("Username eingeben: ")
+sock.connect((HOST, PORT))
+sock.sendall(bytes(name, "utf-8"))
 
 while (data != "exit"):
     data = input("Ich:")
     try:
         # Connect to server and send data
-        sock.connect((HOST, PORT))
-        sock.sendall(bytes(data + "\n", "utf-8"))
+        sock.sendall(bytes(data, "utf-8"))
     except:
         sock.close()
