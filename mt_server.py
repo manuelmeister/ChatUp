dfbf93c746a532b8cf35efc0f26ca42f7c57f29c
gruemschele <news.manuelsworld@gmail.com>
2014-11-25 11:36:07 +0100

    Client List use

--- mt_server.py
+++ mt_server.py
@@ -1,12 +1,12 @@
 import socket, threading, sys
 
 class ChatServer:
-    def __init__(self, port=12345, host=''):
+    def __init__(self, port=12345, host="localhost"):
         threading.Thread.__init__(self)
         self.port = port
         self.host = host
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clients = {}
+        self.users = {}
         self.threads = []
 
         try:
@@ -21,14 +21,18 @@ class ChatServer:
         self.sock.close()
 
     def threadrunner(self,client,addr):
-        name = self.clients[client]
+        name = self.users[client]
         print("Client",name,"connected with ",addr[0],":",str(addr[1]))
         while True:
             data = client.recv(1024)
             if not data:
                 break
-            print(name,":", data.decode("utf-8"))
-            client.sendall(data)
+            print(name.decode("utf-8"),":", data.decode("utf-8"))
+            for user in self.users:
+                if user == client:
+                    return
+                else:
+                    user.sendall(name + b":" + data)
 
         client.close()
 
@@ -36,9 +40,9 @@ class ChatServer:
         print("Waiting for connections on port",self.port)
         while True:
             client, addr = self.sock.accept()
-            name = client.recv(1024).decode("utf-8")
+            name = client.recv(1024)
 
-            self.clients[client] = name
+            self.users[client] = name.decode("utf-8")
 
             threading.Thread(target=self.threadrunner, args=(client,addr)).start()
 
