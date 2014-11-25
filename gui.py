dd81b5ee89f2ca65e4329fff0b6459717ca4d171
gruemschele <news.manuelsworld@gmail.com>
2014-11-25 13:10:31 +0100

    Helper functions

--- gui.py
+++ gui.py
@@ -5,16 +5,21 @@ class ChatGui:
     def __init__(self):
         self.main = Tk()
         self.main.title('ChatUp')
-        self.strInput = str()
         self.txtChat=Text(self.main, height=20, width=50, bg="#bbbbbb")
         self.txtChat.pack(side=TOP)
+        self.strInput = str()
         self.txtInput = Entry(self.main,  width=40, textvariable=self.strInput)
         self.txtInput.pack( side = LEFT)
         self.cmdSubmit = Button(self.main, width=10, command=self.submit, text="Submit")
         self.cmdSubmit.pack(side=RIGHT)
         self.main.mainloop()
 
+    #on button click
     def submit(self):
-        self.txtChat.insert(END,"submit\n")
+        return  self.txtInput.get()
+
+    def writeLine(self, strMessage):
+        self.txtChat.insert(END,strMessage + "\n")
+
 
 #main.mainloop()
