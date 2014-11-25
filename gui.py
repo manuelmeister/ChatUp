630c3593e7397bc2c4a87c15d79fe488a9ee3ad8
Lukas.Walker <lukas.walker@bluewin.ch>
2014-11-25 11:33:20 +0100

    GUI for client

--- gui.py
+++ gui.py
@@ -1,9 +1,14 @@
 from tkinter import *
 
+def submit():
+    print("subit")
+
 main = Tk()
 strInput = str()
-lblChat = Label(main, height=100, width=150, bg="grey")
+lblChat = Text(main, height=20, width=50, bg="#bbbbbb")
 lblChat.pack(side=TOP)
-txtInput = Entry(main, textvariable=strInput)
+txtInput = Entry(main,  width=40, textvariable=strInput)
 txtInput.pack( side = LEFT)
+cmdSubmit = Button(main, width=10, command=submit, text="Submit")
+cmdSubmit.pack(side=RIGHT)
 main.mainloop()
